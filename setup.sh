32e52b5803a3e5a87a87d6676e9091e574384df4
Jason Wallace <jason.wallace@uga.edu>
2025-06-10 10:34:12 -0400

    Update setup.sh to use Sapelo2 data path

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 
 # Script to set up analysis of E coli variation for BINF 8960
 
+source=/scratch/jgwall  # Source directory of files (different than in class so works on Sapelo2 cluster)
+
 # Make subdirectories
 mkdir data docs results
 
 # Copy over data 
-cp -r /work/binf8960/instructor_data/raw_fastq ./data/
+cp -r $source/instructor_data/raw_fastq ./data/
 
 # Make raw data read-only
 chmod -w data/raw_fastq/*.fastq*
